ab7a5f64ffc91b7dddedfd3b465286ed255a8579
Md. Nayeem <renayeem@gmail.com>
2023-08-30 14:52:26 +0600

    sidebar loaded

--- src/App.tsx
+++ src/App.tsx
@@ -4,19 +4,25 @@ import './App.css';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Home from './pages/ContactList';
 import { QueryClient, QueryClientProvider, useQuery } from 'react-query'
+import { DefaultSidebar } from './components/sidebar';
+import { ThemeProvider } from "@material-tailwind/react";
 
 const queryClient = new QueryClient()
 
 function App() {
   return (
     <div className="App">
+
       <QueryClientProvider client={queryClient}>
-        <BrowserRouter>
-          <Routes>
-            <Route path="/" element={<Home />} />
-          </Routes>
-        </BrowserRouter>
+        <ThemeProvider>
+          <BrowserRouter>
+            <Routes>
+              <Route path="/" element={<Home />} />
+            </Routes>
+          </BrowserRouter>
+        </ThemeProvider>
       </QueryClientProvider>
+      <DefaultSidebar />
     </div>
   );
 }
---
+++ src/components/sidebar.tsx
@@ -0,0 +1,30 @@
+import {
+    Card,
+    Typography,
+    List,
+    ListItem,
+    ListItemPrefix,
+    ListItemSuffix,
+    Chip,
+} from "@material-tailwind/react";
+
+
+export function DefaultSidebar() {
+    return (
+        <Card className="h-[calc(100vh-2rem)] w-full max-w-[20rem] p-4 shadow-xl shadow-blue-gray-900/5">
+            <div className="mb-2 p-4">
+                <Typography variant="h5" color="blue-gray">
+                    Sidebar
+                </Typography>
+            </div>
+            <List>
+                <ListItem>
+                    Contact
+                </ListItem>
+                <ListItem>
+                    Charts and Maps
+                </ListItem>
+            </List>
+        </Card>
+    );
+}
