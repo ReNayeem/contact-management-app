ce4499fa94144665b6e5d9021994d64a82ef6851
Md. Nayeem <renayeem@gmail.com>
2023-08-30 13:37:37 +0600

    app setup done

---
+++ src/App.tsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Home from './pages/ContactList';
+import { QueryClient, QueryClientProvider, useQuery } from 'react-query'
+
+const queryClient = new QueryClient()
+
+function App() {
+  return (
+    <div className="App">
+      <QueryClientProvider client={queryClient}>
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<Home />} />
+          </Routes>
+        </BrowserRouter>
+      </QueryClientProvider>
+    </div>
+  );
+}
+
+export default App;
