f22a22ccf108d569ebecf3d7bc7b6d310de0d68a
Md. Nayeem <renayeem@gmail.com>
2023-08-30 14:53:52 +0600

    mock data loaded

---
+++ src/mockData.ts
@@ -0,0 +1,61 @@
+// mockData.ts
+
+export interface Contact {
+    id: number;
+    name: string;
+    email: string;
+    phone: string;
+    address: string;
+    isActive: boolean;
+}
+
+let contacts: Contact[] = [
+    {
+        id: 1,
+        name: 'John Doe',
+        email: 'john@example.com',
+        phone: '123-456-7890',
+        address: '123 Main St, City',
+        isActive: true,
+    },
+    // Add more contacts
+];
+
+export function fetchContacts(): Promise<Contact[]> {
+    return new Promise((resolve) => {
+        setTimeout(() => {
+            resolve(contacts);
+        }, 500);
+    });
+}
+
+export function addContact(newContact: Contact): Promise<void> {
+    return new Promise((resolve) => {
+        setTimeout(() => {
+            newContact.id = contacts.length + 1;
+            contacts.push(newContact);
+            resolve();
+        }, 300);
+    });
+}
+
+export function updateContact(updatedContact: Contact): Promise<void> {
+    return new Promise((resolve) => {
+        setTimeout(() => {
+            const index = contacts.findIndex((contact) => contact.id === updatedContact.id);
+            if (index !== -1) {
+                contacts[index] = updatedContact;
+            }
+            resolve();
+        }, 300);
+    });
+}
+
+export function deleteContact(id: number): Promise<void> {
+    return new Promise((resolve) => {
+        setTimeout(() => {
+            contacts = contacts.filter((contact) => contact.id !== id);
+            resolve();
+        }, 300);
+    });
+}
