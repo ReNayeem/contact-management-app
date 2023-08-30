fdf78b5d518e2443e511056b4b0b3b84f3b16e02
Md. Nayeem <renayeem@gmail.com>
2023-08-30 19:27:49 +0600

    map added

--- src/components/CovidStats.tsx
+++ src/components/CovidStats.tsx
@@ -1,12 +1,28 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import { CartesianGrid, Tooltip, Legend, Line, LineChart, XAxis, YAxis, AreaChart, Area, ResponsiveContainer, RadarChart, PolarGrid, PolarAngleAxis, PolarRadiusAxis, Radar, BarChart, Bar } from 'recharts';
+import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
+import 'leaflet/dist/leaflet.css';
+import icon from 'leaflet/dist/images/marker-icon.png';
+import iconShadow from 'leaflet/dist/images/marker-shadow.png';
+import L from 'leaflet';
+
+let DefaultIcon = L.icon({
+    iconUrl: icon,
+    shadowUrl: iconShadow
+});
+
+L.Marker.prototype.options.icon = DefaultIcon;
 
 interface CountryStats {
     country: string;
     cases: number;
     deaths: number;
     recovered: number;
+    countryInfo: {
+        lat: number;
+        long: number;
+    };
 }
 
 function CovidStats() {
@@ -40,6 +56,27 @@ function CovidStats() {
                 <Line type="monotone" dataKey="deaths" stroke="red" />
                 <Line type="monotone" dataKey="recovered" stroke="#82ca9d" />
             </LineChart>
+            <MapContainer style={{ height: '500px', width: '100%' }} center={[20, 0]} zoom={5}>
+                <TileLayer
+                    attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+                    url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                />
+                {countryData.map((country) => (
+                    <Marker
+                        key={country.country}
+                        position={[country.countryInfo.lat, country.countryInfo.long]}
+                    >
+                        <Popup>
+                            <div>
+                                <h3>{country.country}</h3>
+                                <p>Total Cases: {country.cases}</p>
+                                <p>Total Deaths: {country.deaths}</p>
+                                <p>Total Recovered: {country.recovered}</p>
+                            </div>
+                        </Popup>
+                    </Marker>
+                ))}
+            </MapContainer>
         </div>
     );
 }
